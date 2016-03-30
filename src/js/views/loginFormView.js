8ec15035b1188c34a878b3b01f51a19564999a11
Gustavo Lelis <gustavo.lelis@bloomingdales.com>
2016-03-30 11:06:00 -0300

    seconde

--- src/js/views/loginFormView.js
+++ src/js/views/loginFormView.js
@@ -1,26 +1,20 @@
-define(['marionette',
-		'backbone',
-		'models/loginModel',
-		'text!templates/loginFormTemplate'
-], function(Marionette, Backbone, LoginModel, LoginFormTemplate){
-	var LoginFormView = new Marionette.ItemView.extend({
-		template: LoginFormTemplate,
-		model: new LoginModel(),
-		events: {
-			'click @ui.submit':   'doLogin'
-		},
-		ui: {
-			usernameField: 'input[name="username"]',
-			passwordField: 'input[name="password"]',
-			submit: 'button'
-		},
-		doLogin: function(){
-			this.model.set('username', this.ui.usernameField.val());
-			this.model.set('password', this.ui.passwordField.val());
-			this.model.login();
-		}
-	});
+define(['backbone', 'marionette', 'models/loginModel', 'text!templates/loginFormTemplate'], function(Backbone, Marionette, LoginModel, LoginFormTemplate){
+  return Marionette.ItemView.extend({
+    template: LoginFormTemplate,
+    model: new LoginModel(),
+    events: {
+      'click @ui.submit': 'doLogin'
+    },
+    ui: {
+      usernameField: 'input[name="username"]',
+      passwordField: 'input[name="password"]',
+      submit: 'button'
+    },
+    doLogin: function(){
+      this.model.set('username', this.ui.usernameField.val());
+      this.model.set('password', this.ui.passwordField.val());
+      this.model.login();
+    }
 
-	return LoginFormView;
-
+  });
 });
